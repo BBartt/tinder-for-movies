40cb6d4d6d2b15e8121e8d64239fa1f5bd54eafd
Bartosz <bartek2@onet.eu>
2021-12-01 19:47:49 +0100

    Reinstall node_modules, remove line import '@testing-library/jest-dom';

--- src/tests/components/Button.test.tsx
+++ src/tests/components/Button.test.tsx
@@ -1,6 +1,5 @@
 import { render, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import '@testing-library/jest-dom';
 
 import Button from '../../components/Button';
 
